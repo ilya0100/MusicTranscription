4177227871ab767641a4ec1158ab378e3af2d6fa
Ilya <mr.ilya868@gmail.com>
2022-11-15 09:50:23 +0300

    Init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Automatic music transcription using neural networks',
+    author='ilya',
+    license='MIT',
+)
